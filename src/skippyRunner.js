7440fbe9f7ade441781902fd7d008530bc745a45
Albert Brand <abrand@xebia.com>
2015-06-28 16:39:09 +0200

    Code dedupe in runner, change some output

--- src/skippyRunner.js
+++ src/skippyRunner.js
@@ -22,41 +22,40 @@ let phantomBoot = new Promise((resolve) => {
 });
 
 
-function storeCoverage(coverage, fileName) {
-  coverageOut[getCoverageName(fileName)] = coverage;
-  fs.writeFileSync(config.coveragePath + getCoverageName(fileName), JSON.stringify(coverage), 'utf8');
-}
-
 function getCoverageName(file) {
   return path.parse(file).name + '.coverage.json';
+}
+
+function runPage(indexFileName, evalFn) {
+  return new Promise((resolve) => {
+    ph.createPage((page) => {
+      page.open('http://localhost:' + config.httpServerPort + '/' + indexFileName, () => {
+        page.evaluate(evalFn, resolve);
+      });
+    });
+  });
 }
 
 function doCoverage(srcFiles, testFile) {
   console.log('Get coverage for', testFile);
 
-  let indexFile = runnerTemplate.createIndexFile(srcFiles, testFile);
-  console.log(indexFile);
+  const indexFile = runnerTemplate.createIndexFile(srcFiles, testFile);
 
-  return new Promise((resolve) => {
-    ph.createPage((page) => {
-      page.open('http://localhost:' + config.httpServerPort + '/' + indexFile, () => {
-        page.evaluate(() => {
-          //noinspection JSUnresolvedVariable
-          return __coverage__;
-        }, (result) => {
-          storeCoverage(result, testFile);
-          resolve();
-        });
-      });
-    });
+  return runPage(indexFile, () => {
+    //noinspection JSUnresolvedVariable
+    return __coverage__;
+  }).then((coverage) => {
+    const coverageName = getCoverageName(testFile);
+    coverageOut[coverageName] = coverage;
+    fs.writeFileSync(config.coveragePath + coverageName, JSON.stringify(coverage), 'utf8');
   });
 }
 
 function initCoverage(srcFiles, testFiles) {
   phantomBoot.then(() => {
     let promises = [doCoverage(srcFiles, NO_TEST)];
-    for (let file of testFiles) {
-      promises.push(doCoverage(srcFiles, file));
+    for (let testFile of testFiles) {
+      promises.push(doCoverage(srcFiles, testFile));
     }
 
     Promise.all(promises).then(() => {
@@ -81,43 +80,40 @@ function initCoverage(srcFiles, testFiles) {
         }
       }
 
-      console.log('Source file and their related tests:');
+      console.log('Source files and their related tests:');
       console.log(diffResult);
     });
   });
 }
 
 function runTestInPhantom(testFile) {
-  let indexFile = runnerTemplate.getIndexFileName(testFile);
-  ph.createPage((page) => {
-    page.open('http://localhost:' + config.httpServerPort + '/' + indexFile, () => {
-      page.evaluate(() => {
-        return JSR.results;
-      }, (results) => {
-        let success = true;
-        for (let result of results) {
-          if (result.status === 'passed') {
-            console.log(colors.green(result.description));
-          } else {
-            console.log(colors.red(result.description));
-            success = false;
-          }
-        }
-        console.log('Test ', success ? colors.green(testFile) : colors.red(testFile));
-      });
-    });
+  const indexFileName = runnerTemplate.getIndexFileName(testFile);
+
+  runPage(indexFileName, () => {
+    return JSR.results;
+  }).then((results) => {
+    let success = true;
+    for (let result of results) {
+      if (result.status === 'passed') {
+        console.log(colors.green(result.description));
+      } else {
+        console.log(colors.red(result.description));
+        success = false;
+      }
+    }
+    console.log('Test', success ? 'succeeded: ' + colors.green(testFile) : 'failed: ' + colors.red(testFile));
   });
 }
 
 function runTest(file) {
   phantomBoot.then(() => {
     if (diffResult[file]) {
-      console.log('Source file changed, running related tests: ', diffResult[file]);
+      console.log('Source file changed, run related tests:', diffResult[file]);
       for (let testFile of diffResult[file]) {
         runTestInPhantom(testFile);
       }
     } else {
-      console.log('Test file changed, running only this: ', file);
+      console.log('Test file changed, instrument and run:', file);
       runTestInPhantom(file);
     }
   });
